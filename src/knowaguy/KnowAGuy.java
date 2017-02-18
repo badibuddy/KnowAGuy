3a8116b6c14c2d635ed0209a2875706a00c65019
Badi Weru <badi.weru@gmail.com>
2017-02-18 23:26:46 +0300

    First draft on Registration

--- src/knowaguy/KnowAGuy.java
+++ src/knowaguy/KnowAGuy.java
@@ -5,17 +5,46 @@
  */
 package knowaguy;
 
+import java.util.Scanner;
+
 /**
  *
  * @author bweru
  */
 public class KnowAGuy {
 
+    public static final Scanner scanner = new Scanner( System.in );
+    public static String sel;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print( "\tWelcome to KnowAGuy. \n Kindly select an option to proceed: "
+            + "\n 1. Login"
+            + "\n 2. Register"
+            + "\n *. Quit: \n" );
+        sel = scanner.nextLine();
+        welcomeOptions();
     }
     
+
+    public static String welcomeOptions( ) {
+        switch (sel) {
+             case "1":
+                break;
+            case "2":
+                accountCreation.main(new String[] {""});
+                break;
+            case "*":
+                System.exit(0);
+                break;
+            default:
+                System.err.println("Kindly input relevant choice i.e [1],[2]");
+                String[] params = new String[] {""};
+                KnowAGuy.main(params);
+        }
+
+    return sel;
+    }
 }
