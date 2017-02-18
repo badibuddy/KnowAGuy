aad8c860f9f3412bbef91b10687674f3e95ca4e7
Badi Weru <badi.weru@gmail.com>
2017-02-18 23:52:03 +0300

    Touched on the login module

--- src/knowaguy/login.java
+++ src/knowaguy/login.java
@@ -4,11 +4,26 @@
  * and open the template in the editor.
  */
 package knowaguy;
+import java.util.Scanner;
+
 
 /**
  *
  * @author bweru
  */
 public class login {
-    
+    public static void main(String[] args) {
+        String role = "";
+        System.out.print( "Please enter your user name: " );
+        String uname = KnowAGuy.scanner.nextLine();
+        System.out.print( "Please enter your password: " );
+        String passwd = KnowAGuy.scanner.nextLine();
+        if(uname.equalsIgnoreCase("uname")){
+            role = "vendor";
+        }
+        else {
+            role = "client";        
+        }
+    System.out.print(role);
+    }
 }
